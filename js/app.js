09626abd2f5d5d34aa7a6d1a0bfbab30ae2eb48e
Saif <saif.zsdb@gmail.com>
2024-12-17 21:28:52 +0600

    fynaly added js for gallary

--- js/app.js
+++ js/app.js
@@ -1,10 +1,28 @@
 const defultImg = document.querySelector('.container .gallary .defualt');
 const firstImg = document.querySelector('.container .gallary .mult-gall div:first-child');
-const seconttImg = document.querySelector('');
-const thirdImg = document.querySelector('');
-const forthImg = document.querySelector('');
+const seconttImg = document.querySelector('.container .gallary .mult-gall div:nth-child(2)');
+const thirdImg = document.querySelector('.container .gallary .mult-gall div:nth-child(3)');
+const forthImg = document.querySelector('.container .gallary .mult-gall div:last-child');
 
 firstImg.addEventListener("click", () => {
     defultImg.style.background = "url('../img/product1/img-1.png')";
 
 })
+seconttImg.addEventListener("click", () => {
+    defultImg.style.background = "url('../img/product1/img-2.png')";
+
+
+})
+thirdImg.addEventListener("click", () => {
+    defultImg.style.background = "url(../img/product1/img-3.png)";
+
+})
+forthImg.addEventListener("click", () => {
+    defultImg.style.background = "url('../img/product1/img-4.png')";
+
+
+})
+
+
+
+
