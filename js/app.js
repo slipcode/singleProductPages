b38f0cf842a7ca18cd68de3357107ae01cb8fd7e
Saif <saif.zsdb@gmail.com>
2024-12-17 18:33:17 +0600

    js add for gallary

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const defultImg = document.querySelector('.container .gallary .defualt');
+const firstImg = document.querySelector('.container .gallary .mult-gall div:first-child');
+const seconttImg = document.querySelector('');
+const thirdImg = document.querySelector('');
+const forthImg = document.querySelector('');
+
+firstImg.addEventListener("click", () => {
+    defultImg.style.background = "url('../img/product1/img-1.png')";
+
+})
