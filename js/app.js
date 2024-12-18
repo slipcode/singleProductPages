27efe78b07a3e56375d2bdb22f9ef691292b9e53
Saif <saif.zsdb@gmail.com>
2024-12-18 14:00:20 +0600

    js modification

--- js/app.js
+++ js/app.js
@@ -4,21 +4,26 @@ const seconttImg = document.querySelector('.container .gallary .mult-gall .two')
 const thirdImg = document.querySelector('.container .gallary .mult-gall .three');
 const forthImg = document.querySelector('.container .gallary .mult-gall .four');
 
+const firstImgPath = "url('../img/product1/img-1.png')";
+const seconttImgPath = "url('../img/product1/img-2.png')";
+const thirdImgPath = "url(../img/product1/img-3.png)";
+const forthImgPath = "url('../img/product1/img-4.png')";
+
 firstImg.addEventListener("click", () => {
-    defultImg.style.background = "url('../img/product1/img-1.png')";
+    defultImg.style.background = firstImgPath;
 
 })
 seconttImg.addEventListener("click", () => {
-    defultImg.style.background = "url('../img/product1/img-2.png')";
+    defultImg.style.background = seconttImgPath;
 
 
 })
 thirdImg.addEventListener("click", () => {
-    defultImg.style.background = "url(../img/product1/img-3.png)";
+    defultImg.style.background = thirdImgPath;
 
 })
 forthImg.addEventListener("click", () => {
-    defultImg.style.background = "url('../img/product1/img-4.png')";
+    defultImg.style.background = firstImgPath;
 
 
 })
